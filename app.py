b86de16908cb7b8455b66e80267d6a656c85c7bd
Keyner Gallego <keynergallego43@gmail.com>
2026-06-20 18:08:16 -0500

    Fix registrar cashregister blueprint

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from routes.deliveries import deliveries_bp
 from routes.dashboard import dashboard_bp
 from routes.reports import reports_bp
 from routes.store import store_bp
+from routes.cashregister import cashregister_bp
 import os
 
 # --- Crear instancia de Flask ---
@@ -50,6 +51,7 @@ app.register_blueprint(deliveries_bp, url_prefix='/deliveries')
 app.register_blueprint(dashboard_bp,  url_prefix='/')
 app.register_blueprint(reports_bp,    url_prefix='/reports')
 app.register_blueprint(store_bp, url_prefix='/store')
+app.register_blueprint(cashregister_bp, url_prefix='/cashregister')
 
 # --- Ruta raíz: redirige al dashboard ---
 @app.route('/')
